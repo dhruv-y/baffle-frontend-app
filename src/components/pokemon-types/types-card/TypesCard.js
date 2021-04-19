14b251c9ac9ff96df7acfda318124a416f867715
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-04-19 16:33:30 -0400

    chore: update type card to trigger action

--- src/components/pokemon-types/types-card/TypesCard.js
+++ src/components/pokemon-types/types-card/TypesCard.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import axios from 'axios';
+import { GlobalContext } from '../../../context/GlobalState';
+
 export default function TypesCard({ type }) {
+    const { getAllPokemon } = useContext(GlobalContext);
 
     const getTypeData = async (type) => {
-        const res = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
-        console.log(res.data.pokemon)
+        try {
+            const res = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
+            const pokemons = res.data.pokemon
+            getAllPokemon(pokemons)
+
+        } catch (err) {
+            console.log("error", err);
+            // handle error
+        }
     }
 
     return (
