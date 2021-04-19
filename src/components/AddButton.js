4d5d65cb6ce824a26d1da1d013d924ab86891987
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-04-19 18:55:39 -0400

    chore: added button components for the app

---
+++ src/components/AddButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddButton() {
+    return (
+        <div className="button-container" style={{ textAlign: 'right' }}>
+            <i class="fas fa-plus" style={{ cursor: 'pointer' }}></i>
+        </div>
+    )
+}
---
+++ src/components/CloseButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CloseButton() {
+    return (
+        <div className="button-container" style={{ textAlign: 'right' }}>
+            <i class="fas fa-times" style={{ cursor: 'pointer', color: 'red' }}></i>
+        </div>
+    )
+}
