af1f93a71090685b2f9c7816366416f5bb4aa76d
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-04-21 16:07:15 -0400

    chore: minor updates to folder structure

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import './homepage.style.scss'
 import logo from '../../img/pokedex.png'
 import TypesContainer from '../../components/pokemon-types/types-container/TypesContainer'
-import PokemonContainer from '../../components/pokemons/pokemon-container/PokemonContainer'
+import PokemonContainer from '../../components/pokemon-list/pokemon-container/PokemonContainer'
 import FavoritesContainer from '../../components/favorites/favorites-container/FavoritesContainer'
 import Modal from '../../components/modal/Modal'
 import { GlobalContext } from '../../context/GlobalState';
