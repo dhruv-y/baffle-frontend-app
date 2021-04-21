70936bb42342a16948c72caf4d824797bedc21d9
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-04-21 00:25:03 -0400

    feat: adding pokemon types search filter

--- src/components/pokemon-types/types-container/TypesContainer.js
+++ src/components/pokemon-types/types-container/TypesContainer.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TYPE_DATA from './TypesData'
 import TypesCard from '../types-card/TypesCard'
 import './types-container.style.scss'
 
 export default function TypesContainer() {
-    const [pokeTypes, setPokeTypes] = useState({
-        types: TYPE_DATA
-    });
-    const { types } = pokeTypes
+    const [searchResult, setSearchResult] = useState([])
+    const [searchItem, setSearchItem] = useState("")
+
+    useEffect(() => {
+        const results = TYPE_DATA.filter(({ type }) =>
+            type.includes(searchItem.toLowerCase())
+        );
+        setSearchResult(results);
+    }, [searchItem])
+
+    const handleChange = (e) => {
+        setSearchItem(e.target.value)
+    }
 
     return (
         <div className="types-container">
-            <h2 className="title">Pokemon Types</h2>
+            <input
+                type="text"
+                className="search"
+                placeholder="Search a type..."
+                value={searchItem}
+                onChange={handleChange}
+            />
             {
-                types.map(({ id, type }) => (
+                searchResult.map(({ id, type }) => (
                     <TypesCard key={id} type={type} />
                 ))
             }
-        </div>
+        </div >
     )
 }
