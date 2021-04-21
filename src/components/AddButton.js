e6c48310e72a75bc21aa16e779eeb34792c0b9ac
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-04-21 00:23:17 -0400

    chore: separating button components

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
-export default function AddButton() {
+export default function AddButton({ id, name }) {
+    const { openModal, favorites } = useContext(GlobalContext);
+
+    let favoritedPokemon = favorites.find(p => p.number === id);
+    const favoritesDisabled = favoritedPokemon ? true : false
+
+    const setModalOpen = ({ id, name }, isOpen) => {
+        openModal({
+            name: name,
+            number: id,
+            isOpen: isOpen
+        })
+    }
+
     return (
         <div className="button-container" style={{ textAlign: 'right' }}>
-            <i class="fas fa-plus" style={{ cursor: 'pointer' }}></i>
+            <button
+                onClick={() => setModalOpen({ id, name }, true)}
+                disabled={favoritesDisabled}
+                style={{ border: 0, padding: '5px', background: "transparent" }}
+            >
+                <i className="fas fa-plus" style={{ cursor: 'pointer' }}></i>
+            </button>
         </div>
     )
 }
--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
-export default function CloseButton() {
+export default function CloseButton({ type, id }) {
+    const { closeModal, removePokemonFromFav } = useContext(GlobalContext);
+
     return (
-        <div className="button-container" style={{ textAlign: 'right' }}>
-            <i class="fas fa-times" style={{ cursor: 'pointer', color: 'red' }}></i>
+        <div className="close-button" style={{ textAlign: 'right' }}>
+            {type === "modal" && (
+                <>
+                    <button style={{ border: 0, background: "transparent" }}>
+                        <i className="fas fa-times"
+                            style={{ cursor: 'pointer', color: 'red' }}
+                            onClick={() => closeModal()}>
+                        </i>
+                    </button>
+                </>
+            )
+
+            }
+
+            {type === 'favorites' && (
+                <>
+                    <button style={{ border: 0, background: "transparent" }}>
+                        <i className="fas fa-times"
+                            style={{ cursor: 'pointer', color: 'red' }}
+                            onClick={() => removePokemonFromFav(id)}>
+                        </i>
+                    </button>
+                </>
+            )
+            }
         </div>
     )
 }
